d26f773b2a15e744cc2729601db694ac74f3c98f
antkeyes <keyes.anthony@gmail.com>
2022-01-02 11:03:25 -0500

    added clamp and inRange methods

--- _.js
+++ _.js
@@ -1,5 +1,31 @@
-console.log('hello')
-s
+let _ = {
+    clamp(number, lower, upper) {
+        let lowerClampedValue = Math.max(number, lower);
+        let clampedValue = Math.min(lowerClampedValue, upper);
+
+        return clampedValue;
+    },
+
+    inRange(number, start = 0, end) {
+        if (end == undefined) {
+            end = start;
+            start = 0;
+        }
+        if (start > end) {
+            temp = start;
+            start = end;
+            end = temp;
+        }
+        if (number >= start && number < end) {
+            return true;
+        } else {
+            return false;
+        }
+    },
+    
+};
+
+
 
 
 
